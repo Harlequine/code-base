96552264779d74ac829049df84bc8c753343af0e
Louis Andrei Suba <lasuba1124@gmail.com>
2021-01-31 18:48:46 +0800

    main

---
+++ C++/Linked List Program/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+
+void mainscreen(){
+
+}
+
+
+int main (){
+
+int choice;
+
+    switch (choice)
+    {
+    case 1:
+        cout << "Enter a new Input"
+        break;
+    
+    default:
+        break;
+    }
+
+    while (true){
+        mainscreen();
+        cout << "Enter the number of your choice here: ";
+    }
+
+
+}
