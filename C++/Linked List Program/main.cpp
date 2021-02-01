a7acbc2a9a0c1a468aaa2089e6b59d88faa87719
Louis Andrei Suba <51257898+subalouis@users.noreply.github.com>
2021-02-01 16:50:45 +0800

    functions

--- C++/Linked List Program/main.cpp
+++ C++/Linked List Program/main.cpp
@@ -1,23 +1,45 @@
 #include <iostream>
 #include <stack>
-using namespace std;
+#include <time.h> //time decalarion
+
+time_t timeToday = time (NULL);
+
+void request(){
+
+}
+
+void reservation (){
+
+}
 
 void mainscreen(){
-    cout << "We"
+   std:: cout << "Welcome to CS Cinema!"<<std::endl;
+   std:: cout << "The Current time today is: "; printf("%s", ctime(&timeToday));
 }
+
+// main functionz
 int main (){
-    
     int choice;
-    switch (choice)
-    {
-        case 1:
-        cout << "[1] Reservation"<<endl;
-        break;
-    default:
-        break;
-    }
+
     while (true){
         mainscreen();
-        cout << "Enter the number of your choice here: ";
+        std::cout << "Enter the number of your choice here: "; std::cin>>choice;
+    }
+
+    switch (choice){
+        case 1:
+            std::cout << "[1] Reservation"<<std::endl;
+            reservation ();
+            break;
+        case 2: 
+        std::cout<<"[2] View Available Seats and Schedule"<<std::endl;
+            request();
+            break;
+        case 3: 
+        std::cout<<"[3] Request for new time slot/position"<<std::endl;
+            break;
+        case 4:
+        std::cout<< "[4] Exit Program";
+            return 0;
     }
 }
