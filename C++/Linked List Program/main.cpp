c2e394c09f941f4be0468d3f29f3cb0906b3c25f
Louis Andrei Suba <51257898+subalouis@users.noreply.github.com>
2021-02-01 12:43:09 +0800

    removed excess folder

--- C++/Linked List Program/main.cpp
+++ C++/Linked List Program/main.cpp
@@ -2,30 +2,22 @@
 #include <stack>
 using namespace std;
 
-
 void mainscreen(){
-
+    cout << "We"
 }
-
-
 int main (){
-
-int choice;
-
+    
+    int choice;
     switch (choice)
     {
-    case 1:
-        cout << "Enter a new Input"
+        case 1:
+        cout << "[1] Reservation"<<endl;
         break;
-    
     default:
         break;
     }
-
     while (true){
         mainscreen();
         cout << "Enter the number of your choice here: ";
     }
-
-
 }
